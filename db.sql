7a6eaff93f118aab79bac7afbb75806cbcd67836
seong-soyeon <yesican951011@gmail.com>
2020-08-13 07:19:19 +0900

    db업데이트

--- db.sql
+++ db.sql
@@ -10,7 +10,7 @@ CREATE TABLE article (
     updateDate DATETIME,
     delDate DATETIME,
 	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
-	displayStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+	`articleReply`displayStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
     title CHAR(200) NOT NULL,
     `body` LONGTEXT NOT NULL
 );
@@ -32,7 +32,6 @@ updateDate = NOW(),
 title = '제목1',
 `body` = '내용1';
 displayStatus = 1;
-
 # articleReply 테이블에 테스트 데이터 삽입
 INSERT INTO articleReply
 SET regDate = NOW(),
@@ -48,6 +47,55 @@ ALTER TABLE `at`.`reply` ADD INDEX (`relId`, `relTypeCode`);
 UPDATE reply
 SET relTypeCode = 'article'
 WHERE relTypeCode = '';
+
+# member 테이블 세팅
+DROP TABLE IF EXISTS `member`;
+CREATE TABLE `member` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME,
+    updateDate DATETIME,
+    delDate DATETIME,
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+	authStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+    loginId CHAR(20) NOT NULL UNIQUE,
+    loginPw CHAR(100) NOT NULL,
+    `name` CHAR(20) NOT NULL,
+    `nickname` CHAR(20) NOT NULL,
+    `email` CHAR(100) NOT NULL,
+    `cellphoneNo` CHAR(20) NOT NULL
+);
+
+# member 테이블에 테스트 데이터 삽입
+INSERT INTO `member`
+SET regDate = NOW(),
+updateDate = NOW(),
+loginId = 'admin',
+loginPw = SHA2('admin', 256),
+`name` = '관리자',
+`nickname` = '관리자',
+`email` = '',
+`cellphoneNo` = '';
+
+/* 파일 테이블 생성 */
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file` (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME,
+    updateDate DATETIME,
+    delDate DATETIME,
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+	relTypeCode CHAR(50) NOT NULL,
+	relId INT(10) UNSIGNED NOT NULL,
+    originFileName VARCHAR(100) NOT NULL,
+    fileExt CHAR(10) NOT NULL,
+    typeCode CHAR(20) NOT NULL,
+    type2Code CHAR(20) NOT NULL,
+    fileSize INT(10) UNSIGNED NOT NULL,
+    fileExtTypeCode CHAR(10) NOT NULL,
+    fileExtType2Code CHAR(10) NOT NULL,
+    fileNo TINYINT(2) UNSIGNED NOT NULL,
+    `body` LONGBLOB
+);
 
 SELECT *
 FROM article; 
